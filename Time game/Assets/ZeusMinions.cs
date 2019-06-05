9e0d03cb1d9d5b234fa8fab2e06195815595fee5
Gordon <aardappel156@gmail.com>
2019-06-05 16:30:14 +0200

    Zeus WIP
    Only created the scene so far

---
+++ Time game/Assets/ZeusMinions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class ZeusMinions : MonoBehaviour
+{
+    NavMeshAgent agent;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
