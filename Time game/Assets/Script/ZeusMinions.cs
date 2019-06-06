f60e50630f9acc98dbfb4d269826c73dde6f8d63
Gordon <aardappel156@gmail.com>
2019-06-06 10:24:26 +0200

    lightning complete
    it only miss some art. also moved soms files around

---
+++ Time game/Assets/Script/ZeusMinions.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+/*summary
+ * this is part of the zeus mini-game
+ * it is a navmeshagent that goes to different point on the ground
+ * if it collides with the lightning sphere collider it will be destroyed
+ * 
+*/
+public class ZeusMinions : MonoBehaviour
+{
+    NavMeshAgent agent;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if(agent == null)
+        {
+            Debug.Log("THere is no navmeshagent on this object!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(agent.destination == null || agent.remainingDistance < 1)
+        {
+            agent.destination = new Vector3(Random.Range(-15, 15), 1, Random.Range(-10, 10));
+        }
+      
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Lightning"))
+        {
+            Debug.Log("I fucking died");
+        }
+    }
+}
---
+++ Time game/Assets/Script/ZeusPower.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DigitalRuby.LightningBolt;
+using UnityEngine;
+
+/* summary
+ * This handles everything of the power of zeus
+ * the lightning is a linerenderer and has a sphere trigger on the ground
+ * timebetweenlightning is basicaly like a cooldown
+*/
+public class ZeusPower : MonoBehaviour
+{
+
+    public GameObject thunderPrefab;
+    public float timeBetweenLightning;
+
+    float lightningCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && lightningCooldown < Time.time)
+        {
+            RaycastHit hit;
+            Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+            lightningCooldown = Time.time + timeBetweenLightning;
+
+            if (Physics.Raycast(ray, out hit, 20f))
+            {
+                if(hit.collider != null)
+                {
+                    GameObject temp = Instantiate(thunderPrefab, hit.point, transform.rotation);
+                    Destroy(temp, .2f);
+                    LightningBoltScript lightningbolt = temp.GetComponent<LightningBoltScript>();
+                    //Debug.Log(hit.point);
+                }
+            }
+        }
+
+        Debug.DrawRay(transform.position, Input.mousePosition, Color.black);
+    }
+}
